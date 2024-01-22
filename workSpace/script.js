e651e8cf5ca5d4c58d2b82f131b605531f032b53
Kwazii <146226859+KwaziiMedia@users.noreply.github.com>
2024-01-22 18:04:33 -0500

    first model

--- workSpace/script.js
+++ workSpace/script.js
@@ -1,19 +1,31 @@
-//Number Variables
-const num1 = document.getElementById("num1");
-const num2 = document.getElementById("num2")
-const num3 = document.getElementById("num3")
-const num4 = document.getElementById("num4")
-const num5 = document.getElementById("num5")
-const num6 = document.getElementById("num6")
-const num7 = document.getElementById("num7")
-const num8 = document.getElementById("num8")
-const num9 = document.getElementById("num9")
-const num0   = document.getElementById("num0")
-//Math variables
-const divideBtn = document.getElementById("divide");
-const addBtn = document.getElementById("plus");
-const multiplyBtn = document.getElementById("multiply")
-const subtractBtn = document.getElementById("minus")
-const decimalBtn = document.getElementById("decimal")
-const equalBtn = document.getElementById("equal")
+//userinput
+let firstNumber ;
+let secondNumber ;
+let operator ;
+let result
+//display
+let display = document.getElementById("display").innerText;
 
+function appendNumber(number) {
+    display += number
+    document.getElementById("display").innerText = display
+}
+//appendOperator function
+function appendOperator(operator){
+    if(display.length > 0) {
+        firstNumber = display;
+        operator = operator;
+        display += operator
+        document.getElementById("display").innerText = display
+    }
+}
+//Clear Function
+function clearDisplay(){
+    display = ""
+    document.getElementById("display").innerText = display
+}
+//calculate function
+function calculate(){
+    result = eval(display)
+    document.getElementById("display").innerText = result
+}
